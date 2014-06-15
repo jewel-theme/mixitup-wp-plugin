20f94490956c8643991e37403d84820e922b130a
Harshit <Harshit@harshit-pc>
2014-06-15 17:08:22 +0530

    Initial Setup

---
+++ mixitup-wp-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Mixitup Wordpress Plugin
+ * Plugin URI: http://wordpress.org/plugins/mixitup-wordpress-plugin/
+ * Description: MixItUp is a jQuery plugin providing animated filtering and sorting.
+ * Author: Harshit gupta
+ * Version: 1.0
+ * Author URI: https://github.com/harshit101
+ */
+?>
