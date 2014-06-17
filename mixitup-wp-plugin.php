15c4b4bec8111dee748e587a29592449e2e461ea
Anand <Anand@Unicode-PC>
2014-06-17 15:50:17 +0530

    constant definition

--- mixitup-wp-plugin.php
+++ mixitup-wp-plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Mixitup Wordpress Plugin
  * Plugin URI: http://wordpress.org/plugins/mixitup-wordpress-plugin/
@@ -7,4 +8,43 @@
  * Version: 1.0
  * Author URI: https://github.com/harshit101
  */
+
+/**
+ * @constant MIXIT_PLUGIN_PATH
+ * @description Constant for plugin DIR PATH
+ */
+if (!defined('MIXIT_PLUGIN_PATH'))
+    define('MIXIT_PLUGIN_PATH', plugin_dir_path(__FILE__)) . DIRECTORY_SEPARATOR . 'mixitup-wp-plugin';
+
+/**
+ * @constant MIXIT_PLUGIN_URL 
+ * @description Constant for plugin URL
+ */
+if (!defined('MIXIT_PLUGIN_URL'))
+    define('MIXIT_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+/**
+ * @constant MIXIT_PLUGIN_JS_URL 
+ * @description Constant for all js directory
+ */
+if (!defined('MIXIT_PLUGIN_JS_URL'))
+    define('MIXIT_PLUGIN_JS_URL', MIXIT_PLUGIN_URL . 'assets/js');
+
+/**
+ * @constant MIXIT_PLUGIN_CSS_URL 
+ * @description Constant for all css directory
+ */
+if (!defined('MIXIT_PLUGIN_CSS_URL'))
+    define('MIXIT_PLUGIN_CSS_URL', MIXIT_PLUGIN_URL . 'assets/css');
+
+add_action('admin_menu', 'create_mixit_setting_page');
+
+function create_mixit_setting_page() {
+    add_options_page('Mixitup Options', 'Mixit Options', 'administrator', 'mixitup-options', 'setting_html');
+}
+
+function setting_html() {
+    include_once MIXIT_PLUGIN_PATH . 'inc/options.php';
+}
+
 ?>
